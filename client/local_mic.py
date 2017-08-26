3032124c4fa3c7041ed8d8a9695f1a5c5214d98d
yunxiyinzhe <yunxiyinzhe@gmail.com>
2017-08-26 23:05:18 +0800

    修改测试MIC类方便进行输出测试

--- client/local_mic.py
+++ client/local_mic.py
@@ -4,13 +4,24 @@ A drop-in replacement for the Mic class that allows for all I/O to occur
 over the terminal. Useful for debugging. Unlike with the typical Mic
 implementation, Jasper is always active listening with local_mic.
 """
-
+import logging
+import pyaudio
+import alteration
 
 class Mic:
     prev = None
 
     def __init__(self, speaker, passive_stt_engine, active_stt_engine):
-        return
+        self._logger = logging.getLogger(__name__)
+        self.speaker = speaker
+        self._logger.info("Initializing PyAudio. ALSA/Jack error messages " +
+                          "that pop up during this process are normal and " +
+                          "can usually be safely ignored.")
+        self._audio = pyaudio.PyAudio()
+        self._logger.info("Initialization of PyAudio completed.")
+
+    def __del__(self):
+        self._audio.terminate()
 
     def passiveListen(self, PERSONA):
         return True, "JASPER"
@@ -28,5 +39,8 @@ class Mic:
         self.prev = input
         return input
 
-    def say(self, phrase, OPTIONS=None):
-        print("JASPER: %s" % phrase)
+    def say(self, phrase,
+            OPTIONS=" -vdefault+m3 -p 40 -s 160 --stdout > say.wav"):
+        # alter phrase before speaking
+        phrase = alteration.clean(phrase)
+        self.speaker.say(phrase)
