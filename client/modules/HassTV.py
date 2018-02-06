16aaee9afe2572cdc8835fc50b5266bf02a0a3ce
EnderCui <ender.cui@hotmail.com>
2018-02-06 14:41:43 +0800

    Enhance Hass for TV control.

--- client/modules/HassTV.py
+++ client/modules/HassTV.py
@@ -11,22 +11,32 @@ sys.setdefaultencoding('utf8')
 WORDS = ["HOMEASSISTANT"]
 SLUG = "home_assistant"
 
-PHRASES = [u"我想看", u"遥控", u"关闭"]
-DEVICES = {u"电视": 'switch.tv',
-           u"主页": 'switch.tv_homepage',
-           u"左边": 'switch.tv_left',
-           u"右边": 'switch.tv_right',
-           u"下边": 'switch.tv_down',
-           u"上边": 'switch.tv_up',
-           u"确定": 'switch.tv_confirm',
-           u"后退": 'switch.tv_backward',
-           u"大点声": 'switch.tv_volup',
-           u"小点声": 'switch.tv_voldown',
-           u"儿歌": 'switch.song',
-           u"直播": 'switch.bo'}
+PHRASES = [u"打开", u"关闭", u"我想看", u"遥控"]
+OPERATION = {
+    u"电视": 'switch.tv',
+    u"主页": 'switch.tv_homepage',
+    u"上边": 'switch.tv_up',
+    u"下边": 'switch.tv_down',
+    u"左边": 'switch.tv_left',
+    u"右边": 'switch.tv_right',
+    u"确定": 'switch.tv_confirm',
+    u"后退": 'switch.tv_backward',
+    u"大点声": 'switch.tv_volup',
+    u"小点声": 'switch.tv_voldown',
+    u"儿歌": 'switch.song',
+    u"直播": 'switch.live'}
+
+LOGGER = logging.getLogger(__name__)
+
+
+def isValid(text):
+    for phrases in PHRASES:
+        if text.startswith(phrases):
+            return True
+    return False
+
 
 def handle(text, mic, profile, bot=None):
-    logger = logging.getLogger(__name__)
     '''
     home_assistant:
         Host: "192.168.2.235"
@@ -48,82 +58,128 @@ def handle(text, mic, profile, bot=None):
     headers = {'x-ha-access': password, 'content-type': 'application/json'}
     text = text.strip().replace("，", "").replace("。", "")
     key = text.decode('utf-8')[2:]
-    #cmd = "turn_on" if text.decode('utf-8')[:2] == u"打开" else "turn_off"
     cmd = "turn_off" if text.decode('utf-8')[:2] == u"关闭" else "turn_on"
 
-    if key in DEVICES:
+    if key in OPERATION:
         service = 'switch'
         requestService = url + service + "/" + cmd
-        if key == u"儿歌":
-             try:
-                 data = json.dumps({'entity_id': DEVICES[u"下边"]})
-                 response = post(requestService, data, headers=headers)
-                 time.sleep(1)
-
-                 data = json.dumps({'entity_id': DEVICES[u"右边"]})
-                 response = post(requestService, data, headers=headers)
-                 time.sleep(1)
-
-                 data = json.dumps({'entity_id': DEVICES[u"确定"]})
-                 response = post(requestService, data, headers=headers)
-                 time.sleep(10)
-
-                 data = json.dumps({'entity_id': DEVICES[u"确定"]})
-                 response = post(requestService, data, headers=headers)
-                 time.sleep(20)
-
-                 data = json.dumps({'entity_id': DEVICES[u"上边"]})
-                 response = post(requestService, data, headers=headers)
-                 time.sleep(1)
-
-                 data = json.dumps({'entity_id': DEVICES[u"上边"]})
-                 response = post(requestService, data, headers=headers)
-                 time.sleep(1)
-
-                 data = json.dumps({'entity_id': DEVICES[u"右边"]})
-                 response = post(requestService, data, headers=headers)
-                 time.sleep(1)
-
-                 data = json.dumps({'entity_id': DEVICES[u"右边"]})
-                 response = post(requestService, data, headers=headers)
-                 time.sleep(1)
-
-                 data = json.dumps({'entity_id': DEVICES[u"确定"]})
-                 response = post(requestService, data, headers=headers)
-                 time.sleep(4)
-
-                 data = json.dumps({'entity_id': DEVICES[u"下边"]})
-                 response = post(requestService, data, headers=headers)
-                 time.sleep(1)
-
-                 data = json.dumps({'entity_id': DEVICES[u"下边"]})
-                 response = post(requestService, data, headers=headers)
-                 time.sleep(1)
-
-                 data = json.dumps({'entity_id': DEVICES[u"确定"]})
-                 response = post(requestService, data, headers=headers)
-             except Exception as e:
-                 mic.say(u"不能遥控打开" + text)
-                 logger.error(e)
-        else:
-            if key == u"直播":
-                try:
-                    data = json.dumps({'entity_id': DEVICES[key]})
-                    service = DEVICES[key].split('.')[0]
-                    response = post(url + service + "/" + cmd, data, headers=headers)
-                    logger.debug(response)
-                    mic.say(text)
-                except Exception as e:
-                    mic.say(u"不能遥控打开" + text)
-                    logger.error(e)
-            else:
-                mic.say(u"没听清你要看什么节目")
+        if key == u"儿歌": watchSong(requestService, headers, mic)
+        if key == u"直播": watchLive(requestService, headers, mic)
+        if key == u"主页": watchHomepage(requestService, headers, mic)
+        if key == u"电视": operateTv(requestService, headers, mic, key)
+        if key == u"上边": operateTv(requestService, headers, mic, key)
+        if key == u"下边": operateTv(requestService, headers, mic, key)
+        if key == u"左边": operateTv(requestService, headers, mic, key)
+        if key == u"右边": operateTv(requestService, headers, mic, key)
+        if key == u"大点声": operateTv(requestService, headers, mic, key)
+        if key == u"小点声": operateTv(requestService, headers, mic, key)
     else:
         mic.say(u"我没办法完成" + text + u"操作")
 
 
-def isValid(text):
-    for phrases in PHRASES:
-        if text.startswith(phrases):
-            return True
-    return False
+def watchSong(requestService, headers, mic):
+    try:
+        data = json.dumps({'entity_id': OPERATION[u"下边"]})
+        response = post(requestService, data, headers=headers)
+        time.sleep(1)
+
+        data = json.dumps({'entity_id': OPERATION[u"右边"]})
+        response = post(requestService, data, headers=headers)
+        time.sleep(1)
+
+        data = json.dumps({'entity_id': OPERATION[u"确定"]})
+        response = post(requestService, data, headers=headers)
+        time.sleep(10)
+
+        data = json.dumps({'entity_id': OPERATION[u"确定"]})
+        response = post(requestService, data, headers=headers)
+        time.sleep(20)
+
+        data = json.dumps({'entity_id': OPERATION[u"上边"]})
+        response = post(requestService, data, headers=headers)
+        time.sleep(1)
+
+        data = json.dumps({'entity_id': OPERATION[u"上边"]})
+        response = post(requestService, data, headers=headers)
+        time.sleep(1)
+
+        data = json.dumps({'entity_id': OPERATION[u"右边"]})
+        response = post(requestService, data, headers=headers)
+        time.sleep(1)
+
+        data = json.dumps({'entity_id': OPERATION[u"右边"]})
+        response = post(requestService, data, headers=headers)
+        time.sleep(1)
+
+        data = json.dumps({'entity_id': OPERATION[u"确定"]})
+        response = post(requestService, data, headers=headers)
+        time.sleep(4)
+
+        data = json.dumps({'entity_id': OPERATION[u"下边"]})
+        response = post(requestService, data, headers=headers)
+        time.sleep(1)
+
+        data = json.dumps({'entity_id': OPERATION[u"下边"]})
+        response = post(requestService, data, headers=headers)
+        time.sleep(1)
+
+        data = json.dumps({'entity_id': OPERATION[u"确定"]})
+        response = post(requestService, data, headers=headers)
+    except Exception as e:
+        mic.say(u"不能遥控打开儿歌")
+        LOGGER.error(e)
+
+
+def watchLive(requestService, headers, mic):
+    try:
+        data = json.dumps({'entity_id': OPERATION[u"下边"]})
+        response = post(requestService, data, headers=headers)
+        time.sleep(1)
+
+        data = json.dumps({'entity_id': OPERATION[u"右边"]})
+        response = post(requestService, data, headers=headers)
+        time.sleep(1)
+
+        data = json.dumps({'entity_id': OPERATION[u"确定"]})
+        response = post(requestService, data, headers=headers)
+        time.sleep(10)
+
+        data = json.dumps({'entity_id': OPERATION[u"右边"]})
+        response = post(requestService, data, headers=headers)
+        time.sleep(1)
+
+        data = json.dumps({'entity_id': OPERATION[u"确定"]})
+        response = post(requestService, data, headers=headers)
+    except Exception as e:
+        mic.say(u"不能遥控打开直播")
+        LOGGER.error(e)
+
+
+def watchHomepage(requestService, headers, mic):
+    try:
+        data = json.dumps({'entity_id': OPERATION[u"后退"]})
+        response = post(requestService, data, headers=headers)
+        time.sleep(2)
+
+        data = json.dumps({'entity_id': OPERATION[u"后退"]})
+        response = post(requestService, data, headers=headers)
+        time.sleep(2)
+
+        data = json.dumps({'entity_id': OPERATION[u"后退"]})
+        response = post(requestService, data, headers=headers)
+        time.sleep(2)
+
+        data = json.dumps({'entity_id': OPERATION[u"主页"]})
+        response = post(requestService, data, headers=headers)
+    except Exception as e:
+        mic.say(u"不能遥控打开主页")
+        LOGGER.error(e)
+
+
+def operateTv(requestService, headers, mic, key):
+    try:
+        data = json.dumps({'entity_id': OPERATION[key]})
+        response = post(requestService, data, headers=headers)
+    except Exception as e:
+        mic.say(u"抱歉，无法遥控电视")
+        LOGGER.error(e)
